df01e0f24e2c0bba522578d794a4a857a5179fa5
Eric C <39015447+ricetech@users.noreply.github.com>
2022-12-11 20:11:29 -0500

    Update Team to use UUID

--- src/state/Team.ts
+++ src/state/Team.ts
@@ -2,15 +2,21 @@ import { DataTypes, Model } from "sequelize";
 import { TableDefinition } from "../types/TableDefinition";
 
 export class Team extends Model {
+  declare id: string;
   declare name: string;
   declare color: string;
 }
 
 export const TeamsTableDef: TableDefinition = {
   attributes: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
-      primaryKey: true,
+      allowNull: false,
     },
     color: {
       type: DataTypes.STRING,
